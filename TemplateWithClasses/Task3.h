ed7f806caf230658da80284a9798c704e3e736dd
Andrey Karagodin <shad-karagodin@ya.ru>
2022-03-31 09:00:52 +0300

    cpplint first try

--- TemplateWithClasses/Task3.h
+++ TemplateWithClasses/Task3.h
@@ -1,26 +1,19 @@
 #pragma once
+//Copyright 2022 Andrew Karagodin
 
 #include <iostream>
 #include <string>
 #include <conio.h>
 #include <Windows.h>
-
 using namespace std;
-
 #define KEY_EXIT 27
 #define ARROW_KEY 224
 #define KEY_SPACE 0
-
 #define KEY_ARROW_RIGHT 77
 #define KEY_ARROW_LEFT 75
 
-
-
-class MyTextEditor
-{
-
+class MyTextEditor{
 protected:
-
 	COORD cursorPos;
 	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	DWORD cWrittenChars;
@@ -28,28 +21,20 @@ protected:
 	int baselen;
 	bool selectedChoice;
 	char* Characters;
-
-
 public:
-	MyTextEditor()
-	{
+	MyTextEditor(){
 		len = 1;
 		baselen = len;
 		selectedChoice = 0;
 		cursorPos.X = 0;
 		cursorPos.Y = 0;
 		SetConsoleCursorPosition(hStdOut, cursorPos);
-
 	};
-
-
 	void printValues() {
 		cout << "X: " << cursorPos.X << endl << "Y: " << cursorPos.Y << endl << "len: " << len << endl;
 	}
-
 	void HavaAChoiceThenWork() {
 		char ch = ' ';
-		
 		if (selectedChoice == 1) {
 			cout << "Хотите продолжить редактирование текста?" << endl;
 			cout << "Enter = Да" << endl << "Esc = Нет" << endl;
@@ -64,35 +49,25 @@ public:
 		}
 		else
 			ch = '\n';
-
 		if (ch == '\n') {
 			PrintLenText();
 			WorkWithText();
 		}
-			
-
 	}
-
-	
-
 	void PrintLenText() {
 		system("CLS");
 		SetConsoleCursorPosition(hStdOut, cursorPos);
 		FillConsoleOutputCharacter(hStdOut, (TCHAR)'_', len, cursorPos, &cWrittenChars);  // записывает символ в экранном буфере консоли заданное число раз
 		SetConsoleCursorPosition(hStdOut, cursorPos);
 	}
-
-
 	void WorkWithText() {
 		SetConsoleCP(1251);
 		SetConsoleOutputCP(1251);
 		int zagl = 67; //Заглушка как учили.
 		baselen = len;
 		CONSOLE_SCREEN_BUFFER_INFO cbsi;
-
 		while (zagl != KEY_EXIT && len > 0) {
-			switch (zagl)
-			{
+			switch (zagl){
 			case KEY_ARROW_LEFT:
 				GetConsoleScreenBufferInfo(hStdOut, &cbsi);       // извлекает информацию о заданном экранном буфере консоли
 				cursorPos.X = cbsi.dwCursorPosition.X - 1;        // смещаем текущее положение курсора на символ назад
@@ -115,51 +90,32 @@ public:
 				len--;
 				break;
 			}
-		}
-		
-		
+		}	
 		Characters = new char[baselen + 1];
 		DWORD dwRead;
-		
 		ReadConsoleOutputCharacter(hStdOut, Characters, baselen, cursorPos, &dwRead);
 		Characters[baselen] = '\0';
 		system("CLS");
-
 		cout << "Your final text: " << Characters << endl;
 		len = baselen;
 		system("pause");
 		system("CLS");
-
 		selectedChoice = 1;
 		HavaAChoiceThenWork();
 	}
-
-
-	
-
-
 	friend ostream& operator<<(ostream& out, const MyTextEditor& r);
 	friend istream& operator>>(istream& in, MyTextEditor& r);
-
 };
-
-
-
-
 istream& operator>>(istream& in, MyTextEditor& right) {
 	cout << "Отступ сверху (Y): ";
 	in >> right.cursorPos.Y;
-
 	cout << "Отступ слева (X): ";
 	in >> right.cursorPos.X;
-
 	cout << "Длина текста (len): ";
 	in >> right.len;
-	
 	right.printValues();
 	return in;
 }
-
 ostream& operator<<(ostream& out, const MyTextEditor& r) {
 	out << "Введите значения для отступа слева (x), сверху (y), а также длину текста(len)" << endl;
 	return out;
