e22dc4314443af98c574fae84d204e6155f1fc29
Andrey Karagodin <shad-karagodin@ya.ru>
2022-03-31 12:01:09 +0300

    cpplint

--- TemplateWithClasses/Task3.h
+++ TemplateWithClasses/Task3.h
@@ -22,73 +22,76 @@ int baselen;
 bool selectedChoice;
 char* Characters;
  public:
-	MyTextEditor(){
-	len = 1;
-	baselen = len;
-	selectedChoice = 0;
-	cursorPos.X = 0;
-	cursorPos.Y = 0;
-	SetConsoleCursorPosition(hStdOut, cursorPos);
-	};
+MyTextEditor(){
+len = 1;
+baselen = len;
+selectedChoice = 0;
+cursorPos.X = 0;
+cursorPos.Y = 0;
+SetConsoleCursorPosition(hStdOut, cursorPos);
+};
 void printValues() {
-	cout << "X: " << cursorPos.X << endl << "Y: " << cursorPos.Y << endl << "len: " << len << endl;
-	}
+cout << "X: " << cursorPos.X << endl << "Y: " << cursorPos.Y << endl << "len: " << len << endl;
+}
 void HavaAChoiceThenWork() {
-	char ch = ' ';
-	if (selectedChoice == 1) {
-	cout << "Хотите продолжить редактирование текста?" << endl;
-	cout << "Enter = Да" << endl << "Esc = Нет" << endl;
-	ch = _getche();
-	if (ch == KEY_EXIT) {
-		system("pause");
-	}else
-		if (ch == '\r') {
-			PrintLenText();
-			WorkWithText();
-		}
-	}
- else
-	ch = '\n';
-	if (ch == '\n') {
-		PrintLenText();
-		WorkWithText();
-	}
+char ch = ' ';
+if (selectedChoice == 1) {
+cout << "Хотите продолжить редактирование текста?" << endl;
+cout << "Enter = Да" << endl << "Esc = Нет" << endl;
+ch = _getche();
+if (ch == KEY_EXIT) {
+system("pause");
+}
+else {
+	if (ch == '\r') {
+	PrintLenText();
+	WorkWithText();
+  }
  }
+}
+else {
+ch = '\n';
+}
+if (ch == '\n') {
+PrintLenText();
+WorkWithText();
+ }
+}
 void PrintLenText() {
- system("CLS");
- SetConsoleCursorPosition(hStdOut, cursorPos);
- FillConsoleOutputCharacter(hStdOut, (TCHAR)'_', len, cursorPos, &cWrittenChars);  // записывает символ в экранном буфере консоли заданное число раз
- SetConsoleCursorPosition(hStdOut, cursorPos);
+system("CLS");
+SetConsoleCursorPosition(hStdOut, cursorPos);
+FillConsoleOutputCharacter(hStdOut, (TCHAR)'_', len, cursorPos, &cWrittenChars);
+SetConsoleCursorPosition(hStdOut, cursorPos);
 }
 void WorkWithText() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	int zagl = 67;  //Заглушка как учили.
-	baselen = len;
-	CONSOLE_SCREEN_BUFFER_INFO cbsi;
-	while (zagl != KEY_EXIT && len > 0) {
-	switch (zagl) {
-	case KEY_ARROW_LEFT:
-		GetConsoleScreenBufferInfo(hStdOut, &cbsi);
-		cursorPos.X = cbsi.dwCursorPosition.X - 1;
-		SetConsoleCursorPosition(hStdOut, cursorPos);
-		len++;
-		zagl = 67;
-		break;
-		 case KEY_ARROW_RIGHT:
-			GetConsoleScreenBufferInfo(hStdOut, &cbsi);
-			cursorPos.X = cbsi.dwCursorPosition.X + 1;
-			SetConsoleCursorPosition(hStdOut, cursorPos);
-			len--;
-			zagl = 67;
-			break;
-		default:
-			zagl = _getch();
-			if (zagl == KEY_EXIT || zagl == ARROW_KEY || zagl == KEY_SPACE
-				|| zagl == KEY_ARROW_LEFT || zagl == KEY_ARROW_RIGHT) break;
-				std::cout << (char)zagl;
-				len--;
-				break;
+SetConsoleCP(1251);
+SetConsoleOutputCP(1251);
+int zagl = 67;  //Заглушка как учили.
+baselen = len;
+CONSOLE_SCREEN_BUFFER_INFO cbsi;
+while (zagl != KEY_EXIT && len > 0) {
+switch (zagl) {
+case KEY_ARROW_LEFT:
+GetConsoleScreenBufferInfo(hStdOut, &cbsi);
+cursorPos.X = cbsi.dwCursorPosition.X - 1;
+SetConsoleCursorPosition(hStdOut, cursorPos);
+len++;
+zagl = 67;
+break;
+case KEY_ARROW_RIGHT:
+GetConsoleScreenBufferInfo(hStdOut, &cbsi);
+cursorPos.X = cbsi.dwCursorPosition.X + 1;
+SetConsoleCursorPosition(hStdOut, cursorPos);
+len--;
+zagl = 67;
+break;
+default:
+zagl = _getch();
+if (zagl == KEY_EXIT || zagl == ARROW_KEY || zagl == KEY_SPACE
+|| zagl == KEY_ARROW_LEFT || zagl == KEY_ARROW_RIGHT) break;
+std::cout << (char)zagl;
+len--;
+break;
  }
 }
 Characters = new char[baselen + 1];
@@ -106,19 +109,21 @@ HavaAChoiceThenWork();
 
 friend ostream& operator<<(ostream& out, const MyTextEditor& r);
 friend istream& operator>>(istream& in, MyTextEditor& r);
+
 };
 
 istream& operator>>(istream& in, MyTextEditor& right) {
- cout << "Отступ сверху (Y): ";
- in >> right.cursorPos.Y;
- cout << "Отступ слева (X): ";
- in >> right.cursorPos.X;
- cout << "Длина текста (len): ";
- in >> right.len;
- right.printValues();
- return in;
+cout << "Отступ сверху (Y): ";
+in >> right.cursorPos.Y;
+cout << "Отступ слева (X): ";
+in >> right.cursorPos.X;
+cout << "Длина текста (len): ";
+in >> right.len;
+right.printValues();
+return in;
 }
+
 ostream& operator<<(ostream& out, const MyTextEditor& r) {
- out << "Введите значения для отступа слева (x), сверху (y), а также длину текста(len)" << endl;
- return out;
+out << "Введите значения для отступа слева (x), сверху (y), а также длину текста(len)" << endl;
+return out;
 }
